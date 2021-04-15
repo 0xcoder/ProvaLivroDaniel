8be1d42de9df5b0a1580c604387e8ea7ce8e0a73
Estevao Rodrigues <estevaorssouza@hotmail.com>
2021-04-15 13:09:32 -0300

    ok
    ok

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,54 @@
+﻿using CadastroLivro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroLivro.Controllers
+{
+    public class LivroController : Controller
+    {
+
+        private static IList<Livro> livros = new List<Livro>()
+        {
+             new Livro()
+            {
+                Id = 1,
+                Nome = "Audi a3",
+                Autor = "Azul",
+                Editora = "Stadia",
+                AnoPu = "1000",
+                Genero = "Ação",
+                Idioma = "Português",
+                Sinopse = "Era uma vez....."
+
+            }
+        };
+
+
+        public IActionResult Index()
+    {
+        return View(livros);
+    }
+
+    public IActionResult CadastroLivros()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CadastroLivros(Livro livro)
+    {
+        if (ModelState.IsValid)
+        {
+            livros.Add(livro);
+            livro.Id = livros.Select(a => a.Id).Max() + 1;
+
+            return RedirectToAction(nameof(Index));
+        }
+        return View(livro);
+    }
+
+}
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroLivro.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string AnoPu { get; set; }
+        public string Editora { get; set; }
+        public string Idioma { get; set; }
+        public string Genero { get; set; }
+        public string Sinopse { get; set; }
+
+
+    }
+}
